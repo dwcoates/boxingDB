d0234906ad5001dc7b7b6d162a38825f9545aa09
dwcoates <dodge.w.coates@gmail.com>
2017-07-26 20:53:16 -0400

    update crawler. Basic code

--- boxingCrawler/boxingCrawler/spiders/random_spiders.py
+++ boxingCrawler/boxingCrawler/spiders/random_spiders.py
@@ -8,111 +8,95 @@ import random
 
 import logging
 
-# in case of a failed region name, the request for that region must have
-# failed. Consult log file link to region.
+# in case of a failed boxer name, the request for that boxer must have
+# failed. Consult log file link to boxer.
 logger = logging.getLogger()
 logging.basicConfig(filename='spider_log', level=logging.WARNING)
 
-
+n
 class RandomSpider(Spider):
-    """
-    Novel stochastic approach to scraping all of the boxers. start at
-    prominant fighter, propogate recursively across his set of
-    opponents. Assume relevant boxing world can be described with a single
-    undirected graph. Handle closures by checking for existance in the set of
-    fighter urls.
+    """Novel stochastic approach to scraping all of the boxers. Start at prominant
+    fighter, propogate recursively across his set of opponents. Assume
+    relevant boxing world can be described with a single undirected
+    graph. Handle closures by checking for existance in the set of scraped
+    fighter urls, which are unique, an O(1) process. Mathematical
+    justification for this intuition being reasonable is beyond me at the
+    moment.
     """
     name = "random_spider"
+    boxer_url = "http://boxrec.com/boxer"
     allowed_urls = ["http://boxrec.com/boxer"]
     start_urls = ["http://boxrec.com/boxer/1"] # needs to be updated with a set of initial boxers
+    status = {"boxer" : 0, "error" : 0, "closures" : 0} # should this be somewhere else?
 
     def __init__(self):
         pass
 
     def parse(self, response):
-        usa_territories = self._parse_continent(response)
 
-        num_regions = sum(map(lambda t: len(t[1]), usa_territories))
-        print("Regions to scrape: {0:,}".format(num_regions))
-        time.sleep(3)
+        # unique id for boxer in the URL
+        boxer_id=response.url[response.url.rfind('/')+1:]
+        # popular name, not birth name
+        name=response.xpath(
+                '//*[@id="homeContent"]/div[1]/div/div[2]/div[4]/div[1]/h1/text()')
+        # dob, stance, height, weightclass, etc
+        stats=response.xpath('//*[@id="bo'+ boxer_id + '"]/div[2]/table/tr')
+        # complete professional boxing record
+        fights=response.xpath(
+            '//*[@id="person_bout_history"]/div[6]/div/div/table/tbody')
 
-        regions_scraped = 0
-        regions_missed = 0
-        for terr_name, region_links in usa_territories:
-            for link in region_links:
-                region = {"name": None, "posts": []}
-                scraped = False
-                attempts = 0
-                # make three attempts to make scrape region
-                while(~scraped and attempts <= 3):
-                    try:
-                        region = RegionSpider._get_posts(link)
-                        scraped = True
-                    except requests.ConnectionError:
-                        logger.warn(
-                            "Connection failure while requesting '%s'", link)
-                        print "Waiting in response to connection failure..."
-                        time.sleep(20)
-                    attempts += 1
-                if scraped:
-                    regions_scraped += 1
-                else:
-                    regions_missed += 1
-                print ("region {0:,}/{1:,} {2} at " +
-                       "'{3}'").format(regions_scraped,
-                                       num_regions,
-                                       "scraped" if scraped else "missed",
-                                       link)
+        boxer_name = boxer_name.strip()
 
-                yield {"state": terr_name, "region": region}
+        boxer_stats=[]
+        for stat in boxer_stats:
+            # grab all stats
+            # add boxer_name to fighter_stats
+            pass
 
-        print ("\n\n{0:,}/{1,:} regions successfully " +
-               "extracted. See logs for " +
-               "failures.\n\n").format(regions_scraped, num_regions)
+        boxer_fights=[]
+        for fight in fights:
+            # grab all fights as a table
+            pass
 
-    @staticmethod
-    def _get_posts(link):
-        """
-        Return the list of posts for the politics section for region at link.
-        If region has subregions, return a list of results for those instead.
-        """
-        def structure((ps, sr_name)):
-            return map(lambda p: {'entry': p, 'subregion': sr_name}, ps)
+        # yield all of boxer's info scraped from profile page
+        yield {"id" : boxer_id
+               "name" : boxer_name,
+               "stats" : boxer_stats,
+               "fights" : boxer_fights}
 
-        region_banner = Selector(text=RegionSpider._get_content(link)).xpath(
-            '//*[@id="topban"]/div[1]')
-        region_name = region_banner.xpath('h2/text()')[0].extract()
+        database=None # fixme
+        # recurse spider across opponents
+        for fight in boxer_fights
+            fid = fight["opponent_id"]
+            if fid not in database:
+                yield scrapy.Request(boxer_url+"/"+fid, callback=self.parse)
+            else:
+                self.status["closures"] += 1 # record that we've found a fighter we've seen
 
-        pol = urljoin(link, "/search/pol")
-        subregions = region_banner.xpath('ul/li')
-        if subregions:
-            def make_link(ps):
-                # there's definitely a cleaner way to build this url
-                path = ps.xpath('a/@href').extract()[0].replace("/", "")
-                return pol.replace("/search/", "/search/" + path + "/")
+     def errback_httpbin(self, failure):
+         """
+         Handle request failures
+         """
+        # log all failures
+        self.logger.error(repr(failure))
 
-            def get_name(ps):
-                return ps.xpath('a/@title').extract()[0]
+        # in case you want to do something special for some errors,
+        # you may need the failure's type:
 
-            subregion_names = map(get_name, subregions)
-            pol_links = map(make_link, subregions)
-            postings = map(RegionSpider._grab_posts, pol_links)
+        if failure.check(HttpError):
+            # these exceptions come from HttpError spider middleware
+            # you can get the non-200 response
+            response = failure.value.response
+            self.logger.error('HttpError on %s', response.url)
 
-            # flatten subregion postings
-            posts = sum(map(structure, zip(postings, subregion_names)), [])
-        else:
-            posts = structure((RegionSpider._grab_posts(pol), None))
+        elif failure.check(DNSLookupError):
+            # this is the original request
+            request = failure.request
+            self.logger.error('DNSLookupError on %s', request.url)
 
-        return {'name': region_name, 'posts': posts}
+        elif failure.check(TimeoutError, TCPTimedOutError):
+            request = failure.request
+            self.logger.error('TimeoutError on %s', request.url)
 
-    @staticmethod
-    def _get_content(link):
-        """
-        Politely request the page content at link.
-        """
-        content = requests.get(link).text
 
-        x = 3 + 2 * random.random()
-        time.sleep(x)
-
-        return content
+        failed_lookups.append(failure.request.url)
