eb023ed2d7f492146c2f461633a2f89e8c63eb4e
dwcoates <dodge.w.coates@gmail.com>
2017-07-26 22:44:54 -0400

    add tomorrow's code

--- boxingCrawler/boxingCrawler/spiders/random_spiders.py
+++ boxingCrawler/boxingCrawler/spiders/random_spiders.py
@@ -2,8 +2,6 @@ from scrapy import Spider
 from scrapy.selector import Selector
 
 import requests
-from urlparse import urlparse, urljoin
-import time
 import random
 
 import logging
@@ -13,7 +11,6 @@ import logging
 logger = logging.getLogger()
 logging.basicConfig(filename='spider_log', level=logging.WARNING)
 
-n
 class RandomSpider(Spider):
     """Novel stochastic approach to scraping all of the boxers. Start at prominant
     fighter, propogate recursively across his set of opponents. Assume
@@ -25,17 +22,31 @@ class RandomSpider(Spider):
     """
     name = "random_spider"
     boxer_url = "http://boxrec.com/boxer"
-    allowed_urls = ["http://boxrec.com/boxer"]
-    start_urls = ["http://boxrec.com/boxer/1"] # needs to be updated with a set of initial boxers
-    status = {"boxer" : 0, "error" : 0, "closures" : 0} # should this be somewhere else?
+    allowed_urls = [boxer_url]
+    start_urls = [boxer_url + "/348759"] # canelo alverez
+    state = {
+        "boxers" : {},           # boxer count by weight class
+        "request errors" : 0,    # boxer request error count
+        "closures" : 0,          # cyclic closure count
+        "total closures" : 0,    # 'fighters with no unseen opponents' count
+        "ftable dim errors" : 0, # unexpected fight table dimension count
+        }
+
+    # should this be somewhere else?
+    expected_ftable_dim=11
+    ftable_failure_threshold=0.005 # 0.5%
 
     def __init__(self):
         pass
 
     def parse(self, response):
+        def grab_boxer_id(url):
+            return url[url.rfind('/')+1:]
 
-        # unique id for boxer in the URL
-        boxer_id=response.url[response.url.rfind('/')+1:]
+        # print status every 100 boxers
+        if sum(state.boxers.values()) % 100 == 0:
+            self.print_state()
+
         # popular name, not birth name
         name=response.xpath(
                 '//*[@id="homeContent"]/div[1]/div/div[2]/div[4]/div[1]/h1/text()')
@@ -45,18 +56,43 @@ class RandomSpider(Spider):
         fights=response.xpath(
             '//*[@id="person_bout_history"]/div[6]/div/div/table/tbody')
 
-        boxer_name = boxer_name.strip()
 
+        boxer_name = name.strip()
+        boxer_id = grab_boxer_id(response.url) # unique id for boxer from URL
         boxer_stats=[]
-        for stat in boxer_stats:
+        boxer_fights=[]
+
+        # build boxer stats
+        for stat in stats:
             # grab all stats
+            # will use a dict of callbacks, indexed by stats table entry name
             # add boxer_name to fighter_stats
             pass
 
-        boxer_fights=[]
-        for fight in fights:
-            # grab all fights as a table
-            pass
+        # build boxer_fights
+        if len(fights) != expected_ftable_dim: # check fight table dimensions
+            self.state["ftable dim errors"] += 1
+            # log error
+            if not self.acceptable_ftable_errors():
+                # log failure
+                # raise exception
+                pass
+        else:
+            fights = [f.xpath('tr[1]/td') for f in fights]
+            for fight in fights:
+                # grab all fights as a table
+                # fight[0] = index
+                # fight[1] = date/calander
+                # fight[2] = opponent name/link
+                # fight[3] = opponent record
+                # fight[4] = location
+                # fight[5] = result?
+                # fight[6] = ?
+                # fight[7] = ?
+                # fight[8] = ?
+                # fight[9] = ?
+                # fight[10] = ?
+                pass
 
         # yield all of boxer's info scraped from profile page
         yield {"id" : boxer_id
@@ -64,19 +100,35 @@ class RandomSpider(Spider):
                "stats" : boxer_stats,
                "fights" : boxer_fights}
 
+        # update state after successfully yielding the boxer to database
+        # 'some weightclass' comes from boxer_stats
+        self.state['boxers']["some weightclass"] = self.state['boxers'].get("some wc", 0) + 1
+
         database=None # fixme
+        num_closures=0
         # recurse spider across opponents
         for fight in boxer_fights
             fid = fight["opponent_id"]
             if fid not in database:
+                closures += 1
                 yield scrapy.Request(boxer_url+"/"+fid, callback=self.parse)
             else:
-                self.status["closures"] += 1 # record that we've found a fighter we've seen
+                self.state["closures"] += 1 # record that we've found a fighter we've seen
 
-     def errback_httpbin(self, failure):
-         """
-         Handle request failures
-         """
+        # record when a fighter has no unseen opponents
+        # this means we've bottomed out on this depth-first traversal
+        if num_closures = len(boxer_fights):
+            self.state["total closures"] += 1
+
+    def print_state(self):
+        # print number closures, boxers, errors, etc
+        # called infrequently to give snapshot of traversal pattern
+        pass
+
+    def errback_httpbin(self, failure):
+        """
+        Handle request failures
+        """
         # log all failures
         self.logger.error(repr(failure))
 
@@ -100,3 +152,8 @@ class RandomSpider(Spider):
 
 
         failed_lookups.append(failure.request.url)
+
+    def acceptable_ftable_errors(self):
+        # if greater than some number of fighters so far and greater beyond
+        # some threshold of error percentages, return false. Else, true
+        pass
